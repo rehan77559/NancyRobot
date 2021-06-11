5b8b482561b704badc73bc0bfd161b8a21807551
ᴠᴀꜱᴜ <79586348+xxvasu@users.noreply.github.com>
2021-06-11 10:09:16 +0530

    Update disasters.py

--- LaylaRobot/modules/disasters.py
+++ LaylaRobot/modules/disasters.py
@@ -628,10 +628,10 @@ def devlist(update: Update, context: CallbackContext):
 #  ❍ /gbanlist*:* Outputs a list of gbanned users
 
 # *Global Blue Text*
-#  ❍ /gignoreblue*:* <word>*:* Globally ignorea bluetext cleaning of saved word across LaylaRobot.
+#  ❍ /gignoreblue*:* <word>*:* Globally ignorea bluetext cleaning of saved word across NancyRobot.
 #  ❍ /ungignoreblue*:* <word>*:* Remove said command from global cleaning list
 
-# *yone Core*
+# *★NสŇcץ★ Core*
 # *Owner only*
 #  ❍ /send*:* <module name>*:* Send module
 #  ❍ /install*:* <reply to a .py>*:* Install module 
